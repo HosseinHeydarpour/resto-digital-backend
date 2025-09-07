60e9df9e126c586b3d0a46b4471409a471748892
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-07 23:50:20 +0330

    feat: add User and Restaurant models

---
+++ model/restaurantModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const restaurantSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Restaurant name is a required field."],
+      trim: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: [true, "Mobile number is a required field."],
+      unique: true,
+      trim: true,
+    },
+    address: {
+      street: { type: String, trim: true },
+      city: { type: String, trim: true },
+      state: { type: String, trim: true },
+      zipCode: { type: String, trim: true },
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    staff: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+module.exports = Restaurant;
