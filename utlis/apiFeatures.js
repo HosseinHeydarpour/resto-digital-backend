c8c39c1aa0592a334af9c655051f1c57c2a24281
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-08 12:07:21 +0330

    feat: implement filtering

---
+++ utlis/apiFeatures.js
@@ -0,0 +1,19 @@
+class APIFeatures {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryStr };
+    const excludeFileds = ["page", "sort", "limit", "fields"];
+    excludeFileds.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+}
