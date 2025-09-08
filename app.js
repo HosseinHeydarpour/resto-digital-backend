51af10b45caf50bc382234fef8b4a43385735d67
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-08 10:19:45 +0330

    feat: implement global error handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const restaurantRouter = require("./routes/restaurant.routes");
 
 const app = express();
 
@@ -12,3 +13,14 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
+
+// =========================
+// **** Route Handlers ****
+// =========================
+app.use("/api/v1/restaurants", restaurantRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} on this sever`, 404));
+});
+
+app.use(globalErrorHandler);
