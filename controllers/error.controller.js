3cd636b963d85cd046064e2f8f7eed806f39acbf
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-24 21:03:04 +0330

    feat: handle invalid ID or invalid fields

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,3 +1,12 @@
+const AppError = require("../utlis/appError");
+
+const handleCastErrorDB = (err) => {
+  // path is the name of the field, value is the problem we passed in
+  const message = `Invalid ${err.path}: ${err.value}`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -10,10 +19,11 @@ const sendErrorDev = (err, res) => {
 const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
-      status: err.message,
+      status: err.status,
       message: err.message,
     });
   } else {
+    // Coding error
     console.log("Error ", err);
     res.status(500).json({
       status: "error",
@@ -29,6 +39,14 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    error.message = err.message; // copy message explicitly
+    error.name = err.name; // copy name explicitly
+    error.path = err.path; // copy path
+    error.value = err.value; // copy value
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
