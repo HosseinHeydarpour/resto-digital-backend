ee1d70868d730e6bb68693909f7e66e451209699
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-26 22:31:04 +0330

    feat: add jwt token for user signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
 const User = require("../model/user.model");
+
 const catchAsync = require("../utlis/catchAsync");
 
+const jwt = require("jsonwebtoken");
+
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    mobileNumber: req.body.mobileNumber,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     staus: "success",
+    token: token,
     data: {
       user: newUser,
     },
