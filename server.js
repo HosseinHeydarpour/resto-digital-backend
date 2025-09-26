004964f09fda990ff26e8a08c446118871f1435b
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-26 21:40:55 +0330

    feat: handle unhandled rejections

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port : ${port} ✅`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
