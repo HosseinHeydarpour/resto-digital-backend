dcdacdff31fe9099275c01b942f5034906fc0e0c
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-26 21:50:39 +0330

    fix: add password confirm field to user

--- model/user.model.js
+++ model/user.model.js
@@ -2,46 +2,47 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      required: [true, "First name is a required field."],
-      trim: true,
-    },
-    lastName: {
-      type: String,
-      required: [true, "Last name is a required field."],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Email is a required field."],
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Password is a required field."],
-    },
-    mobileNumber: {
-      type: String,
-      required: [true, "Mobile number is a required field."],
-      trim: true,
-    },
-    restaurant: {
-      type: Schema.Types.ObjectId,
-      ref: "Restaurant",
-    },
-    role: {
-      type: String,
-      enum: ["owner", "staff"],
-      default: "owner",
-    },
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: [true, "First name is a required field."],
+    trim: true,
   },
-  { timestamps: true }
-);
+  lastName: {
+    type: String,
+    required: [true, "Last name is a required field."],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is a required field."],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is a required field."],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Password confirm is a required field."],
+  },
+  mobileNumber: {
+    type: String,
+    required: [true, "Mobile number is a required field."],
+    trim: true,
+  },
+  restaurant: {
+    type: Schema.Types.ObjectId,
+    ref: "Restaurant",
+  },
+  role: {
+    type: String,
+    enum: ["owner", "staff"],
+    default: "owner",
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 
