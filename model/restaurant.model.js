6ebe4c4a6f302cf217fc8c2054c3fe662631f2db
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-26 21:57:04 +0330

    fix: fix formatting

--- model/restaurant.model.js
+++ model/restaurant.model.js
@@ -4,61 +4,58 @@ const Schema = mongoose.Schema;
 
 const menuSchema = require("./menu.model");
 
-const restaurantSchema = new Schema(
-  {
-    name: {
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Restaurant name is a required field."],
+    trim: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: [true, "Mobile number is a required field."],
+    unique: true,
+    trim: true,
+  },
+  landLine: [
+    {
       type: String,
-      required: [true, "Restaurant name is a required field."],
-      trim: true,
-    },
-    mobileNumber: {
-      type: String,
-      required: [true, "Mobile number is a required field."],
       unique: true,
       trim: true,
     },
-    landLine: [
-      {
-        type: String,
-        unique: true,
-        trim: true,
-      },
-    ],
-    address: {
-      street: { type: String, trim: true },
-      city: { type: String, trim: true },
-      state: { type: String, trim: true },
-      zipCode: { type: String, trim: true },
-    },
-    owner: {
+  ],
+  address: {
+    street: { type: String, trim: true },
+    city: { type: String, trim: true },
+    state: { type: String, trim: true },
+    zipCode: { type: String, trim: true },
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  staff: [
+    {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
-    staff: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
-    socialMedia: [
-      {
-        type: String,
-        trim: true,
-      },
-    ],
-    workingHours: {
+  ],
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  socialMedia: [
+    {
       type: String,
       trim: true,
     },
-    menus: [menuSchema],
+  ],
+  workingHours: {
+    type: String,
+    trim: true,
   },
-  { timestamps: true }
-);
+  menus: [menuSchema],
+});
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
